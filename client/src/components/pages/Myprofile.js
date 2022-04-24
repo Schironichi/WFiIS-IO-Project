f9005ecc10dc11148f8d6fe16496632efd32e7c7
kogelbezmogel <85304480+kogelbezmogel@users.noreply.github.com>
2022-04-24 21:40:35 +0200

    Powiadomienia, Ogłoszenia

--- client/src/components/pages/Myprofile.js
+++ client/src/components/pages/Myprofile.js
@@ -9,11 +9,11 @@ class Myprofile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: { value: "Login", readonly: true },
-      firstName: { value: "Imie", readonly: true },
-      lastName: { value: "Nazwisko", readonly: true },
-      email: { value: "Email", readonly: true },
-      password: { value: "Hasło", readonly: true },
+      userName: "Login",
+      firstName: "Imie",
+      lastName: "Nazwisko",
+      email: "Email",
+      password: "Hasło",
       serverFirstName: "",
       serverLastName: ""
     };
@@ -24,13 +24,8 @@ class Myprofile extends React.Component {
     let data = (await fetch("http://localhost:5000/api/getUserData")).json();
     this.setState( {serverFirstName: (await data).firstName} );
     this.setState( {serverLastName: (await data).lastName} );
-    this.setState( { firstName: {value: (await data).firstName, readonly: true}} );
-    this.setState( { lastName: {value: (await data).lastName, readonly: true}} );
-  }
-
-  editInputClick( property ) {
-    property.readonly = false
-    this.setState( property );
+    this.setState( { firstName: (await data).firstName } );
+    this.setState( { lastName: (await data).lastName } );
   }
 
   changeInput( evt, property ) {
@@ -47,51 +42,47 @@ class Myprofile extends React.Component {
       <>
         <div class='up'>
           <div class='dane'>
-            <h2> Profil użytkownika {this.state.serverFirstName} {this.state.serverLastName} </h2>
+            <h2> Twoje Dane {this.state.serverFirstName} {this.state.serverLastName} </h2>
 
             <InputInProfile
               type='text'
               label='Login'
-              objectAttr={this.state.userName}
+              value={this.state.userName}
               onChange={ (evt) => this.changeInput(evt, this.state.userName) }
-              onClick={ () => this.editInputClick(this.state.userName) }
             />
 
             <InputInProfile 
               type='text'
               label='Imie' 
-              objectAttr={this.state.firstName} 
+              value={this.state.firstName} 
               onChange={ (evt) => this.changeInput(evt, this.state.firstName) } 
-              onClick={ () => this.editInputClick(this.state.firstName) }
             />
 
             <InputInProfile 
               type='text'
               label='Nazwisko' 
-              objectAttr={this.state.lastName} 
+              value={this.state.lastName} 
               onChange={ (evt) => this.changeInput(evt, this.state.lastName) } 
-              onClick={ () => this.editInputClick(this.state.lastName) }
             />
 
             <InputInProfile 
               type='email'
               label='Email' 
-              objectAttr={this.state.email} 
+              value={this.state.email} 
               onChange={ (evt) => this.changeInput(evt, this.state.email) } 
-              onClick={ () => this.editInputClick(this.state.email) }
             />
 
             <InputInProfile 
               type='password'
               label='Hasło' 
-              objectAttr={this.state.password} 
+              value={this.state.password} 
               onChange={ (evt) => this.changeInput(evt, this.state.password) } 
-              onClick={ () => this.editInputClick(this.state.password) }
             />
 
             <p>
               <button type='button' onClick={ () => this.saveAllClick(this) } > Save </button>
             </p>
+
           </div>
             
           <div class='dodaj'>
@@ -99,7 +90,12 @@ class Myprofile extends React.Component {
           </div>
 
         </div>
-        <div class='down'>
+
+        <div style={ {clear: 'both'} }> </div>
+        
+        <div class='down' style={ {marginBottom: '100px'} }>
+          <Announcements />
+          <Messages />
         </div>
         <Footer/>
       </>
@@ -109,6 +105,20 @@ class Myprofile extends React.Component {
 
 
 class InputInProfile extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {readonly: true, checked: false};
+  }
+
+  checkBoxChange() {
+    if( ! this.state.checked )
+      this.setState( {readonly: false, checked: true} )
+    else
+      this.setState( {readonly: true, checked: false })
+  }
+
+
   render() {
     return (
       <>
@@ -121,11 +131,11 @@ class InputInProfile extends React.Component {
           <div className='input_profileInput_div' style={{float: 'left', background: '#61b547'}}>
             <input 
               type = {this.props.type} 
-              value = {this.props.objectAttr.value}
-              readOnly = {this.props.objectAttr.readonly} 
+              value = {this.props.value}
+              readOnly = {this.state.readonly} 
               onChange = { evt => this.props.onChange(evt) }
             /> 
-            <button type='button' onClick={this.props.onClick}> Edit </button>
+            <label> <input type='checkbox' onChange={ () => this.checkBoxChange(this) }/> Edytuj </label>
           </div>
 
           <div className='clear_div' style={{clear: 'both'}}></div>
@@ -134,5 +144,111 @@ class InputInProfile extends React.Component {
     );
   }
 }
+
+
+class Announcements extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { keys: [0], values: ["Puste Pole"] };
+    this.announcements = null;
+  }
+
+  createNotices() {
+    this.announcements = this.state.keys.map( (num) =>
+    <li key={ num.toString() } style={ {background: 'blue', width: '100%'} } >
+      <fieldset style={ {background: 'green'} } >
+        
+        <div style={ {float: 'left', width: '70%'} }>
+          <div> <h1> Ogłoszenie {num + 1} </h1> </div>
+          <div>
+            <p> { this.state.values[num] } </p>
+          </div>
+        </div>
+
+        <div style={ {float: 'left'} }>
+          <button type='button'> Edytuj </button>
+          <button type='button'> Usuń </button>
+        </div>
+
+        <div style={ {clear: 'both'} }></div>
+      </fieldset>
+    </li>
+    );
+  }
+
+  componentDidMount() {
+    //send request for notices data
+    let numbers = [0, 1, 2];
+    let data = [ "Lorem Ipsum1", "Lorem Ipsum2", "Lorem Ipsum3" ];
+    
+    this.setState( { keys: numbers, values: data}, () => this.createNotices(this) );
+  }
+
+  render() {
+    return (
+      <>
+        <h2> Twoje ogłoszenia </h2>
+        <ul style={ {background: 'red', width: '100%'} }>
+          {this.announcements}
+        </ul>
+      </>
+    );
+  }
+}
+
+
+class Messages extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { keys: [0], values: ["Puste Pole"] };
+    this.messages = null;
+  }
+
+  createNotices() {
+    this.messages = this.state.keys.map( (num) =>
+    <li key={ num.toString() } style={ {background: 'blue', width: '100%'} } >
+      <fieldset style={ {background: 'green'} } >
+        
+        <div style={ {float: 'left', width: '70%'} }>
+          <div> <h1> powiadomienie {num + 1} </h1> </div>
+          <div>
+            <p> { this.state.values[num] } </p>
+          </div>
+        </div>
+
+        <div style={ {float: 'left'} }>
+          <button type='button'> Potwierdź </button>
+          <button type='button'> Anuluj </button>
+        </div>
+
+        <div style={ {clear: 'both'} }></div>
+      </fieldset>
+    </li>
+    );
+  }
+
+  componentDidMount() {
+    //send request for messages data
+    let numbers = [0, 1, 2];
+    let data = [ "Lorem Ipsum1", "Lorem Ipsum2", "Lorem Ipsum3" ];
+    
+    this.setState( { keys: numbers, values: data}, () => this.createNotices(this) );
+  }
+
+  render() {
+    return (
+      <>
+        <h2> Twoje powiadomienia </h2>
+        <ul style={ {background: 'red', width: '100%'} }>
+          {this.messages}
+        </ul>
+      </>
+    );
+  }
+}
+
 
 export default Myprofile;
